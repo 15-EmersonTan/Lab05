30b2ce3749cf874f44e7eab978879de684877bcb
15-EmersonTan <emersontan1410022@gmail.com>
2022-10-06 12:08:16 +0800

    Add more coins , Win lose scene ,Player , Timer , Collision , Tags
    All this stuff added

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int Score;
+    public GameObject Player;
+    public GameObject ScoreText;
+    public GameObject Timertext;
+    public static float timeremaining;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeremaining = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.GetComponent<Text>().text = "Score : " + Score;
+        Timertext.GetComponent<Text>().text = "Timer : " + timeremaining.ToString("F2");
+
+        timeremaining -= Time.deltaTime;
+
+        if (timeremaining <= 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+
+        if (Score == 60)
+        {
+            SceneManager.LoadScene("GameWinScene");
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Score+= 10;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+
+    }
+}
---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
